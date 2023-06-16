168fb18b87c2c645e9abf7dac7e7c0edf820d341
DESKTOP-EQAJO7D\DRAGON <ninessander720@gmail.com>
2023-06-16 21:01:03 +0100

    main

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import React, { Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import store from "./reducers";
+import { Provider } from "react-redux";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <Suspense
+    fallback={
+      <div id="sus-fallback">
+        <h1>Loading</h1>
+      </div>
+    }
+  >
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Suspense>
+);
